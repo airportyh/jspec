1542fd49fc29fd86ddee41fb4a7001977c1bdb30
TJ Holowaychuk <tj@vision-media.ca>
2008-10-28 14:49:00 -0700

    - Changed reporting by should_equal and should_not_equal due to specific values not always being available, each assertion should not respond with its own error message

--- README.txt
+++ README.txt
@@ -1,3 +1,35 @@
 
- TODO: this file...
- TODO: drupal module of RSpec AND protolib
+= JSpec
+
+  JSpec is a minimalistic JavaScript test driven development framework,
+  providing both simple installation as well as usage.
+
+== TODO
+
+  * host it so people put uri in script tag... (from github)
+  * textmate bundle
+  * screencast / vision page
+
+== Examples
+
+  with (JSpec('shopping cart')) {                         
+
+		before_each (function(){            
+			this.cart = new Cart();
+		});                                 
+
+		it ('Should have no products to begin with', function(){      
+			this.cart.products.should_equal(0);      
+		});                                 
+
+		it ('Should add products correctly', function(){      
+			this.cart.addProduct('cookies');
+			this.cart.addProduct('more cookies');
+			this.cart.addProduct('lots of cookies!!!');
+			this.cart.products.should_equal(3);
+		});                                                               
+	}
+	
+== Copyright 
+
+	2008 TJ Holowaychuk (MIT Licensed)
--- test/test.js
+++ test/test.js
@@ -1,52 +1,26 @@
 
-with (JSpec('first suite')) {
-	before (function(){
-		console.log('before');            
-	});                                 
-	                                    
-	before_each (function(){            
-		console.log('before each');       
-	});                                 
-	                                    
-	it ('Should pass', function(){      
-		'test'.should_equal('test');      
-	});                                 
-	                                    
-	it ('Should fail', function(){      
-		'test'.should_not_equal('test');  
-	});                                 
-                                      
-	after (function(){                  
-		console.log('after');             
-	});                                 
-	                                    
-	after_each (function(){             
-		console.log('after each');        
-	});                                 
+with (JSpec('suite 1')) {
+	it ('Should pass', function(){
+		'foo'.should_equal('foo');
+	});
+	
+	it ('Should fail', function(){
+		'foo'.should_equal('bar');
+	});
 }
 
-with (JSpec('second suite')) {
-	before (function(){
-		console.log('before');            
-	});                                 
-	                                    
-	before_each (function(){            
-		console.log('before each');       
-	});                                 
-	                                    
-	it ('Should pass', function(){      
-		'test'.should_equal('test');      
-	});                                 
-	                                    
-	it ('Should fail', function(){      
-		'test'.should_not_equal('test');  
-	});                                 
-                                      
-	after (function(){                  
-		console.log('after');             
-	});                                 
-	                                    
-	after_each (function(){             
-		console.log('after each');        
-	});                                 
+with (JSpec('suite 2')) {
+	it ('Should fail', function(){
+		'hello'.should_equal('world');
+	});
+	
+	it ('Should fail', function(){
+		v = 0;
+		v.should_equal(10);
+	});
+	
+	it ('Should fail', function(){
+		v = ['a', 'b', 'c'];
+		v.length.should_not_equal(3);
+	});
 }
