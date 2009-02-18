df0ee1562bc4225358e766ddeda2511acc1f3939
TJ Holowaychuk <tj@vision-media.ca>
2009-02-17 17:33:44 -0800

    - Fixed Spec#failure

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
 
 describe 'Jspec' 
-  it 'should test your codez'
+  it 'should pass'
     'test'.should_equal('test')
   end
   
-  it 'should also test your codes'
+  it 'should fail'
     'test'.should_not_equal('test')
+  end
+  
+  it 'should fail with one faulty assertion'
+    'test'.should_equal('test')
+    'test'.should_equal('foo')
   end
 end
