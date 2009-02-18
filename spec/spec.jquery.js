b11c2fc4485b1987d7d7d0a0ae4528f37d572612
TJ Holowaychuk <tj@vision-media.ca>
2009-02-18 15:54:46 -0800

    - Added some jQuery matchers

--- spec/spec.jquery.js
+++ spec/spec.jquery.js
@@ -2,8 +2,23 @@
 describe 'jQuery matchers' 
   
   it 'have_selector'
-    this.element('#jspec-report').should_have_selector('.passes')
-    this.element('#jspec-report').should_not_have_selector('.foo')
+    $('<p class="article"><h2>Test</h2></p>').should_have_selector('h2')
+  end
+  
+  it 'have_text'
+    this.element('<p>Test</p>').should_have_text('Test')
+  end
+  
+  it 'have_html'
+    this.element('<p><em>Test</em></p>').should_have_html('<em>Test</em>')
+  end
+    
+  it 'have_value'
+    this.element('<input type="textfield" value="cookies">').should_have_value('cookies')
+  end
+  
+  it 'have_class'
+    this.element('<input type="textfield"  class="cookies">').should_have_class('cookies')
   end
   
 end
