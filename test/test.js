272c9bcca97afa72bca3963bd225e76db409374c
TJ Holowaychuk <tj@vision-media.ca>
2009-02-17 17:42:19 -0800

    - Added displaying of assertion counts

--- test/test.js
+++ test/test.js
@@ -1,15 +1,27 @@
 
-describe 'Jspec' 
+describe 'Positive specs' 
+  
   it 'should pass'
     'test'.should_equal('test')
   end
   
+  it 'should pass with several assertions'
+    'foo'.should_equal('foo')
+    'bar'.should_equal('bar')
+    'bar'.should_not_equal('foo')
+  end
+  
+end
+
+describe 'Negative specs'
+
   it 'should fail'
     'test'.should_not_equal('test')
   end
-  
+
   it 'should fail with one faulty assertion'
     'test'.should_equal('test')
     'test'.should_equal('foo')
   end
+  
 end
